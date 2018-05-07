b708a4d5bd8ed2347f255715789e728746d69c8a
Nahidul Hasan <nahidul.cse@gmail.com>
2018-05-07 12:25:36 +0600

    Added elixir mix example

---
+++ elixir-mix/hello_world/lib/hello_world.ex
@@ -0,0 +1,23 @@
+defmodule HelloWorld do
+  @moduledoc """
+  Documentation for HelloWorld.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HelloWorld.hello
+      :world
+
+  """
+
+  def div(a, b) do
+    a / b
+  end
+
+  def hello do
+    :HelloWorld
+  end
+end
